956e4cb4265fefb7545bee9e48bdd9e3d58bdf54
rjema <rachelnyonje@gmail.com>
2026-02-26 21:30:25 -0500

    Updated Q4

--- A1Q4/se3s03a1q4/src/main/java/division/Divide.java
+++ A1Q4/se3s03a1q4/src/main/java/division/Divide.java
@@ -1,6 +1,14 @@
 package division; 
 
 public class Divide {
+    // divide function in first TDD iteration
+    public double old_divide(double numerator, double denominator) {
+        if (denominator == 0.0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return numerator / denominator;
+    }
+
     public double divide(double numerator, double denominator){
         if (denominator == 0){
             throw new ArithmeticException("Cannot divide by 0.");
